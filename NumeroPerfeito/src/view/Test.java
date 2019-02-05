e4e14902429d4950512b3cf50f8b0d84f15cd99d
Biffyclyro <joao-senna1@hotmail.com>
2019-02-05 13:41:20 -0200

    Add files via upload

--- NumeroPerfeito/src/view/Test.java
+++ NumeroPerfeito/src/view/Test.java
@@ -10,7 +10,7 @@ public class Test {
     private static void rangeNum(int x){
         for(int i=0; i<=x; i++){            
             if(Comparador.numPerfeito(i)!=0){
-                System.out.println(i + " É um numero perfeito");
+                System.out.println(i + " E um numero perfeito.");
             }
         }
     }
